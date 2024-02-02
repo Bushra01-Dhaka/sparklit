87462a78c1f5151cc1e2fc8aded818f3b70ff6d6
Bushra01-Dhaka <humayraanjum87@gmail.com>
2024-02-02 11:30:11 +0600

    Responsive Banner Section and also installed Swiper and cretaed Slider component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,14 +4,14 @@ import {motion} from 'framer-motion';
 import img from "../../assets/bannerBg.png";
 const Banner = () => {
     return (
-        <div style={{backgroundImage: `url(${img})`}} className="pt-[50px] flex flex-col lg:flex-row-reverse justify-end bg-fixed bg-opacity-40 items-end hero min-h-screen p-8 gap-4 text-center">
+        <div style={{backgroundImage: `url(${img})`}} className="pt-[150px] flex flex-col lg:flex-row-reverse lg:justify-end bg-fixed bg-opacity-40 lg:items-end hero min-h-screen p-8 gap-4 text-center">
             <motion.div animate={{ y: -50, scale:1}} initial={{scale: 0}}    className="flex-1">
                 <h1 className="text-5xl lg:text-8xl font-bold uppercase text-center lg:text-end text-black">Quotes that spark inspiration</h1>
             </motion.div>
             <div className="flex-1">
                 
-               <div className="flex justify-end">
-               <Link className="lg:text-end "><motion.button  animate={{ y: -50, scale:1}} initial={{scale: 0}} transition={{delay: 1}}  className=" text-center mx-auto p-6 text-xl w-[150px] h-[150px] rounded-xl text-white shadow-xl bg-[#FFA447] hover:bg-[#F28585]   font-semibold uppercase">Explore <FaArrowDown className="text-center mx-auto my-4"></FaArrowDown></motion.button></Link>
+               <div className="flex justify-center lg:justify-end">
+               <Link className="lg:text-end md:text-center "><motion.button  animate={{ y: -50, scale:1}} initial={{scale: 0}} transition={{delay: 1}}  className=" text-center mx-auto p-6 text-xl  lg:w-[150px] lg:h-[150px] rounded-full text-white shadow-xl bg-[#FFA447] hover:bg-[#F28585]   font-semibold uppercase">Explore <FaArrowDown className="text-center mx-auto my-4"></FaArrowDown></motion.button></Link>
                </div>
             </div>
         </div>
--- src/Reused/Navbar.jsx
+++ src/Reused/Navbar.jsx
@@ -1,23 +1,38 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const NavItem = <>
+    <li className="border-0 shadow-[#FFA447] hover:border-2 hoer:bg[#FFA447] rounded-full mr-2 shadow-xl text-black"><Link>All Quotes</Link></li>
+    <li className="border-0 shadow-[#FFA447] hover:border-2 hoer:bg[#FFA447] rounded-full mr-2 shadow-xl text-black"><Link>Popular</Link></li>
+    <li className="border-0 shadow-[#FFA447] hover:border-2 hoer:bg[#FFA447] rounded-full mr-2 shadow-xl text-black"><Link>Signin/Register</Link></li>
+  </>
   return (
-    <div className="md:max-w-screen-xl mx-auto bg-transparent fixed p-6 z-20 ">
-       <div className="flex lg:flex-row justify-between items-center gap-4">
-       <div className="logo">
-        <Link><h2 className="text-3xl text-black">Spark<span className="font-bold text-[#FFA447]">lit</span></h2></Link>
-       </div>
-       
-       <div className="">
-        <ul className="flex space-x-4">
-        <li className="border-2 rounded-[50%] p-4 hover:border-[#FFA447] shadow-lg"><Link>All</Link></li>
-        <li className="border-2 rounded-full p-4 hover:border-[#FFA447] shadow-lg"><Link>Famous</Link></li>
-        <li className="border-2 rounded-full p-4 hover:border-[#FFA447] shadow-lg"><Link>SignIn/Register</Link></li>
-        </ul>
-       </div>
-       </div>
-
+    <div className="navbar fixed z-20 md:max-w-screen-xl mx-auto p-2">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-screen h-screen pt-4">
+      <div className="text-center mx-auto space-y-4 text-white">
+    <li className="text-center bg-[#FFA447] rounded-full p-2"><Link>All Quotes</Link></li>
+    <li className="bg-[#FFA447] rounded-full p-2"><Link>Popular</Link></li>
+    <li className="bg-[#FFA447] rounded-full p-2"><Link>Signin/Register</Link></li>
+  </div>
+      </ul>
     </div>
+    {/* logo */}
+    <Link><h2 className="text-3xl text-black hover:shadow-xl hover:rounded-full hover:shadow-[#FFA447]">Spark<span className="text-[#FFA447] font-bold ">lit</span></h2></Link>
+  </div>
+  <div className="navbar-end hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 border-0">
+      {NavItem}
+    </ul>
+  </div>
+  {/* <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div> */}
+</div>
   );
 };
 
